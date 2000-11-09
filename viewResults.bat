e0b0d9c44fc7798a9a3a98ad9b02113ffb730068
Gary L Peskin <garyp@apache.org>
2000-11-09 08:48:30 +0000

    Allow path names with embedded spaces.

--- viewResults.bat
+++ viewResults.bat
@@ -14,32 +14,32 @@ goto done
 
 :start
 @REM must have name of a results.xml file
-if '%1' == '' goto usage
-if '%1' == '-h' goto usage
-if '%1' == '-H' goto usage
-if '%1' == '-?' goto usage
+if "%1" == "" goto usage
+if "%1" == "-h" goto usage
+if "%1" == "-H" goto usage
+if "%1" == "-?" goto usage
 @echo %0 beginning...
 
 @REM Trickery to guess appropriate location of java.exe program
-if '%JAVA_HOME%' == '' set JAVA_EXE=java
-if not '%JAVA_HOME%' == '' set JAVA_EXE=%JAVA_HOME%\bin\java
+if "%JAVA_HOME%" == "" set JAVA_EXE=java
+if not "%JAVA_HOME%" == "" set JAVA_EXE=%JAVA_HOME%\bin\java
 
 @REM Same logic as runtest.bat
 @REM @todo improve so all relevant batch/shell files use same logic -sc
-if '%JARDIR%' == '' echo viewResults.bat must have JARDIR set!
-if '%JARDIR%' == '' goto done
+if "%JARDIR%" == "" echo viewResults.bat must have JARDIR set!
+if "%JARDIR%" == "" goto done
 
 @REM If JARDIR set, put those references first then default classpath
-if not '%JARDIR%' == '' set TEST_CP=%JARDIR%\testxsl.jar;%JARDIR%\xerces.jar;%JARDIR%\xalan.jar;%JARDIR%\bsf.jar;%JARDIR%\js.jar;%CLASSPATH%
+if not "%JARDIR%" == "" set TEST_CP=%JARDIR%\testxsl.jar;%JARDIR%\xerces.jar;%JARDIR%\xalan.jar;%JARDIR%\bsf.jar;%JARDIR%\js.jar;%CLASSPATH%
 
 @REM Set our output filename
-if '%2' == '' set ROUT=results.html
-if not '%2' == '' set ROUT=%2
-if '%VSXSL%' == '' set VSXSL=viewResults.xsl
+if "%2" == "" set ROUT=results.html
+if not "%2" == "" set ROUT=%2
+if "%VSXSL%" == "" set VSXSL=viewResults.xsl
                              
 @REM @todo find OS-independent way to send 'all remaining args' instead of just up to %9
-echo %JAVA_EXE% %JAVA_OPTS% -classpath %TEST_CP% org.apache.xalan.xslt.Process -in %1 -xsl %VSXSL% -out %ROUT% %3 %4 %5 %6 %7 %8 %9
-%JAVA_EXE% %JAVA_OPTS% -classpath %TEST_CP% org.apache.xalan.xslt.Process -in %1 -xsl %VSXSL% -out %ROUT% %3 %4 %5 %6 %7 %8 %9
+echo "%JAVA_EXE%" %JAVA_OPTS% -classpath "%TEST_CP%" org.apache.xalan.xslt.Process -in "%1" -xsl "%VSXSL%" -out "%ROUT%" %3 %4 %5 %6 %7 %8 %9
+"%JAVA_EXE%" %JAVA_OPTS% -classpath "%TEST_CP%" org.apache.xalan.xslt.Process -in "%1" -xsl "%VSXSL%" -out "%ROUT%" %3 %4 %5 %6 %7 %8 %9
 
 :done
 @echo %0 complete!
