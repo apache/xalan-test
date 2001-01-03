10788e579b0d05c467866999b602491eae5c8257
Scott Boag <sboag@apache.org>
2001-01-03 10:26:58 +0000

    Sax-2-Sax and DOM-2-DOM configurations, that are set up to pass with zero failures, in order to work as clean regression tests.

---
+++ d2d.bat
@@ -0,0 +1,2 @@
+call runtest ConformanceTest -load d2d.prop 
+call viewResults file:///e:\xml-xalan\test\results-d2d\ConfTest.xml
---
+++ s2s.bat
@@ -0,0 +1,2 @@
+call runtest ConformanceTest -load s2s.prop 
+call viewResults file:///e:\xml-xalan\test\results-s2s\ConfTest.xml
