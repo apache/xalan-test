242b1a920d1e444128ebd73581376f7497587850
Shane Curcuru <curcuru@apache.org>
2001-01-04 19:22:27 +0000

    Automatically call viewResults.bat after running test as a convenience

--- ConformanceTest.bat
+++ ConformanceTest.bat
@@ -10,8 +10,15 @@
 if '%1' == '-crimson' set DASHCRIMSON=-crimson
 if '%1' == '-crimson' shift
 set END_PKG=xsl
-@echo Wrapper using '%DASHCRIMSON% xsl.ConformanceTest -load ConformanceTest.properties  arg1 arg2...'
+@echo Wrapper using '%DASHCRIMSON% xsl.ConformanceTest -load ConformanceTest.properties %1 %2 %3 %4 %5 %6 %7 %8 %9'
 call runtest.bat %DASHCRIMSON% ConformanceTest -load ConformanceTest.properties %1 %2 %3 %4 %5 %6 %7 %8 %9
 set END_PKG=
-set DASHCRIMSON=
+
+@REM Automatically attempt to style the results into HTML
+@REM Note: this is optional, but frequent enough to just try it here
+@REM Call with no args to get default processing; note that if 
+@REM  you've overridden logFile, this may not work
+call viewResults.bat %DASHCRIMSON% results-conf\ConformanceTest.xml results-conf\ConformanceTest.html
+
 :end
+set DASHCRIMSON=
--- runharness.bat
+++ runharness.bat
@@ -4,9 +4,20 @@
 @REM	Author: Shane_Curcuru@lotus.com
 
 :start
+@REM Pass along -crimson if it's the first arg
+if '%1' == '-crimson' set DASHCRIMSON=-crimson
+if '%1' == '-crimson' shift
 set END_PKG=xsl
-@echo Harness Wrapper using 'xsl.XSLTestHarness Harness.properties'
-@echo Running multiple tests= from Harness.properties now...
-call runtest.bat XSLTestHarness Harness.properties %2 %3 %4 %5 %6 %7 %8 %9
+@echo Harness Wrapper using '%DASHCRIMSON% xsl.XSLTestHarness Harness.properties %1 %2 %3 %4 %5 %6 %7 %8 %9'
+call runtest.bat %DASHCRIMSON% XSLTestHarness Harness.properties %1 %2 %3 %4 %5 %6 %7 %8 %9
 set END_PKG=
+
+@REM Automatically attempt to style the results into HTML
+@REM Note: this is optional, but frequent enough to just try it here
+@REM Call with no args to get default processing; note that if 
+@REM  you've overridden logFile, this may not work
+call viewHarnessResults.bat  %DASHCRIMSON% results-api\Harness.xml results-api\Harness.html
+
 :end
+set DASHCRIMSON=
+
