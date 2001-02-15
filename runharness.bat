f4794a690325814b5816e9d6894499a369faae50
Shane Curcuru <curcuru@apache.org>
2001-02-15 20:37:28 +0000

    Takes -xalanj1 flag for compatibility testing

--- runharness.bat
+++ runharness.bat
@@ -4,12 +4,24 @@
 @REM	Author: Shane_Curcuru@lotus.com
 
 :start
-@REM Pass along -crimson if it's the first arg
+@REM Pass along -crimson if it's the very first arg
 if '%1' == '-crimson' set DASHCRIMSON=-crimson
 if '%1' == '-crimson' shift
+
+@REM Use alternate Harness*.properties files for 1.x/2.x testing
+set HARNESSP=Harness.properties
+if '%1' == '-xalanj1' set HARNESSP=Harnessxalanj1.properties
+if '%1' == '-xalanj1' shift
+
+@REM EXTRA_CP is used to add xalanj1compat.jar to the start of cp
+@REM Only actually necessary for testing backwards compatibility
+@REM  layer of 2.x, but shouldn't hurt in other cases
+if "%JARDIR%" == "" set EXTRA_CP=..\java\build\xalanj1compat.jar
+if not "%JARDIR%" == "" set EXTRA_CP=%JARDIR%\xalanj1compat.jar
+
 set END_PKG=xsl
-@echo Harness Wrapper using '%DASHCRIMSON% xsl.XSLTestHarness Harness.properties %1 %2 %3 %4 %5 %6 %7 %8 %9'
-call runtest.bat %DASHCRIMSON% XSLTestHarness Harness.properties %1 %2 %3 %4 %5 %6 %7 %8 %9
+@echo Harness Wrapper using '%DASHCRIMSON% xsl.XSLTestHarness %HARNESSP% %1 %2 %3 %4 %5 %6 %7 %8 %9'
+call runtest.bat %DASHCRIMSON% XSLTestHarness %HARNESSP% %1 %2 %3 %4 %5 %6 %7 %8 %9
 set END_PKG=
 
 @REM Automatically attempt to style the results into HTML
