9fb6902e29851756c4d27b88001a584cae7cf2c9
Shane Curcuru <curcuru@apache.org>
2001-06-25 14:57:51 +0000

    Update to use new StylesheetTestletDriver; note END_PKG has been removed; Also update to use TransformWrapper flavors instead of deprecated ProcessorWrapper flavors

--- d2d.bat
+++ d2d.bat
@@ -1,2 +1,3 @@
-call runtest ConformanceTest -load d2d.prop 
+@REM Update for StylesheetTestletDriver/trax.dom 25-Jun-01 -sc 
+call runtest StylesheetTestletDriver -load d2d.prop 
 call viewResults file:///e:\xml-xalan\test\results-d2d\ConfTest.xml
--- s2s.bat
+++ s2s.bat
@@ -1,2 +1,3 @@
-call runtest ConformanceTest -load s2s.prop 
+@REM Update for StylesheetTestletDriver/trax.sax 25-Jun-01 -sc 
+call runtest StylesheetTestletDriver -load s2s.prop 
 call viewResults file:///e:\xml-xalan\test\results-s2s\ConfTest.xml
