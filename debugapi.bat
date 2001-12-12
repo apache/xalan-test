111f3bcc35d00dd724c8024394e2c4128f3451ab
Shane Curcuru <curcuru@apache.org>
2001-12-12 22:35:26 +0000

    Cheap way to run individual tests without using Ant; handy for running in debuggers

---
+++ debugapi.bat
@@ -0,0 +1,13 @@
+@echo off
+@goto start
+@REM	Name:   debugapi.bat
+@REM	Author: Shane_Curcuru@us.ibm.com
+@REM Wrapper for running tests without Ant in a debugger
+@REM Usage: debugapi.bat org.apache.qetest.package.TestName [other options]
+
+:start
+set APICP=..\java\build\xalan.jar;..\java\build\xml-apis.jar;..\java\bin\xerces.jar;java\build\testxsl.jar;%CLASSPATH%
+@echo Executing: java -classpath %APICP% %1 -load debugapi.properties %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath %APICP% %1 -load debugapi.properties %2 %3 %4 %5 %6 %7 %8 %9
+set APICP=
+:end
