0abe542436be33e767ea92de1c269d4d1add967b
Paul Dick <pauldick@apache.org>
2002-06-14 15:13:23 +0000

    Updated default source

--- java/src/org/apache/qetest/dtm/TestDTMIterator.java
+++ java/src/org/apache/qetest/dtm/TestDTMIterator.java
@@ -109,15 +109,15 @@ static final String[] TYPENAME=
 		if(argv.length<1)
 		{
 			String defaultSource=
-				"<?xml version=\"1.0\"?>\n"+
-				"<Document xmlns:x=\"www.x.com\" a1=\"hello\" a2=\"goodbye\">"+
-				"<!-- Default test document -->"+
-				"<?api a1=\"yes\" a2=\"no\"?>"+
-				"<A><B><C><D><E><F/></E></D></C></B></A>"+
-				"<Aa/><Ab/><Ac><Ac1/></Ac>"+
-				"<Ad xmlns:xx=\"www.xx.com\" xmlns:y=\"www.y.com\" xmlns:z=\"www.z.com\">"+
-				"<Ad1/></Ad>"+
-				"</Document>";
+ 		"<?xml version=\"1.0\"?>\n"+
+ 		"<Document xmlns:d=\"www.d.com\" a1=\"hello\" a2=\"goodbye\">"+
+ 		"<!-- Default test document -->"+
+ 		"<?api a1=\"yes\" a2=\"no\"?>"+
+ 		"<A><!-- A Subtree --><B><C><D><E><F xmlns:f=\"www.f.com\" a1=\"down\" a2=\"up\"/></E></D></C></B></A>"+
+ 		"<Aa/><Ab/><Ac><Ac1/></Ac>"+
+ 		"<Ad xmlns:Ad=\"www.Ad.com\" xmlns:y=\"www.y.com\" xmlns:z=\"www.z.com\">"+
+ 		"<Ad1/></Ad>"+
+ 		"</Document>";
 
 			source=new StreamSource(new StringReader(defaultSource));
 		}
