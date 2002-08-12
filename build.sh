611e3be653b1fcfed7b53f40de745e0032107a04
Morris Kwan <mkwan@apache.org>
2002-08-12 14:29:50 +0000

    Fix build problem with test target on Unix.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#	Name:   runtest.sh
+#	Name:   build.sh
 #	Author: Shane Curcuru
 
-echo "runtest.sh beginning..."
+echo "build.sh beginning..."
 
 if [ "$1" = "-h" ] ; then
     echo build.sh - executes Xalan Java-based test automation
@@ -14,21 +14,23 @@ fi
 
 # If PARSER_JAR is not set, default to xercesImpl.jar
 if [ "$PARSER_JAR" = "" ] ; then
-    PARSER_JAR=xercesImpl.jar
+    PARSER_JAR=../java/bin/xercesImpl.jar
+fi
+
+if [ "$XML_APIS_JAR" = "" ]; then
+    XML_APIS_JAR=../java/bin/xml-apis.jar
 fi
 
 if [ "$ANT_HOME" = "" ] ; then
   # try to find ANT
   if [ -d /opt/ant ] ; then 
     ANT_HOME=/opt/ant
-  fi
-
-  if [ -d ${HOME}/opt/ant ] ; then 
+  elif [ -d ${HOME}/opt/ant ] ; then 
     ANT_HOME=${HOME}/opt/ant
+  else
+     # Otherwise, just default the one over in java
+     ANT_HOME=../java
   fi
-
-  # Otherwise, just default the one over in java
-  ANT_HOME=../java
 fi
 
 if [ "$JAVA_HOME" != "" ] ; then
@@ -41,40 +43,40 @@ else
   fi
 fi
  
-# If JARDIR is set, prepend all .jars there to our classpath
-if [ "$JARDIR" != "" ] ; then
-    DIRLIBS=${JARDIR}/*.jar
-    for i in ${DIRLIBS}
-    do
-        if [ "$i" != "${DIRLIBS}" ] ; then
-            _ANT_CP="$i":$_ANT_CP
-        fi
-    done
-fi
-
 # add in the dependency .jar files (copied from ant)
-_ANT_CP=$_ANT_CP:$ANT_HOME/bin/ant.jar
 DIRLIBS=${ANT_HOME}/lib/*.jar
+_ANT_CP=$ANT_HOME/bin/ant.jar
 for i in ${DIRLIBS}
 do
     # if the directory is empty, then it will return the input string
-    # this is stupid, so case for it
+    # this is stupid, so check for it
     if [ "$i" != "${DIRLIBS}" ] ; then
         _ANT_CP=$_ANT_CP:"$i"
     fi
 done
 
-if [ "$CLASSPATH" != "" ] ; then
-  _ANT_CP=$_ANT_CP:$CLASSPATH
+# If JARDIR is set, prepend all .jars there to our classpath
+if [ "$JARDIR" != "" ] ; then
+    CLASSPATH=${_ANT_CP}:${CLASSPATH}
+
+    DIRLIBS=${JARDIR}/*.jar
+    for i in ${DIRLIBS}
+    do
+        if [ "$i" != "${DIRLIBS}" ] ; then
+            CLASSPATH="$i":${CLASSPATH}
+        fi
+    done
+else
+    CLASSPATH=${CLASSPATH}:${_ANT_CP}:${PARSER_JAR}:${XML_APIS_JAR}
 fi
 
 if [ "$JAVA_HOME" != "" ] ; then
   if test -f $JAVA_HOME/lib/tools.jar ; then
-    _ANT_CP=$_ANT_CP:$JAVA_HOME/lib/tools.jar
+    CLASSPATH=${CLASSPATH}:${JAVA_HOME}/lib/tools.jar
   fi
 
   if test -f $JAVA_HOME/lib/classes.zip ; then
-    _ANT_CP=$_ANT_CP:$JAVA_HOME/lib/classes.zip
+    CLASSPATH=${CLASSPATH}:${JAVA_HOME}/lib/classes.zip
   fi
 else
   echo "Warning: JAVA_HOME environment variable is not set."
@@ -92,7 +94,10 @@ if [ "$JIKESPATH" != "" ] ; then
   fi
 fi
 
-echo Running: $JAVACMD -classpath "$_ANT_CP" -Dant.home="${ANT_HOME}" $ANT_OPTS org.apache.tools.ant.Main "$@"
-$JAVACMD -classpath "$_ANT_CP" -Dant.home="${ANT_HOME}" $ANT_OPTS org.apache.tools.ant.Main "$@"
+# also pass along the selected parser to Ant
+ANT_OPTS="${ANT_OPTS} -Dparserjar=${PARSER_JAR}"
 
-echo "runtest.sh complete!"
+echo Running: $JAVACMD ${JAVA_OPTS} -classpath "${CLASSPATH}" -Dant.home="${ANT_HOME}" $ANT_OPTS org.apache.tools.ant.Main "$@"
+$JAVACMD ${JAVA_OPTS} -classpath "${CLASSPATH}" -Dant.home="${ANT_HOME}" $ANT_OPTS org.apache.tools.ant.Main "$@"
+
+echo "build.sh complete!"
