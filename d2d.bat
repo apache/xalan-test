02a8ac2998602ebfc26b948c001c8964ac116c81
Brian James Minchau <minchau@apache.org>
2004-02-24 21:05:40 +0000

    Apache License 2.0 update

--- d2d.bat
+++ d2d.bat
@@ -1,3 +1,19 @@
+@rem
+@rem ==========================================================================
+@rem = Copyright 2004 The Apache Software Foundation.
+@rem =
+@rem = Licensed under the Apache License, Version 2.0 (the "License");
+@rem = you may not use this file except in compliance with the License.
+@rem = You may obtain a copy of the License at
+@rem =
+@rem =     http://www.apache.org/licenses/LICENSE-2.0
+@rem =
+@rem = distributed under the License is distributed on an "AS IS" BASIS,
+@rem = WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem = See the License for the specific language governing permissions and
+@rem = limitations under the License.
+@rem ==========================================================================
+@rem
 @REM Update for StylesheetTestletDriver/trax.dom 25-Jun-01 -sc 
 call runtest StylesheetTestletDriver -load d2d.prop 
 call viewResults file:///e:\xml-xalan\test\results-d2d\ConfTest.xml
--- debugconf.bat
+++ debugconf.bat
@@ -1,5 +1,22 @@
 @echo off
 @goto start
+rem
+rem ==========================================================================
+rem = Copyright 2004 The Apache Software Foundation.
+rem =
+rem = Licensed under the Apache License, Version 2.0 (the "License");
+rem = you may not use this file except in compliance with the License.
+rem = You may obtain a copy of the License at
+rem =
+rem =     http://www.apache.org/licenses/LICENSE-2.0
+rem =
+rem = Unless required by applicable law or agreed to in writing, software
+rem = distributed under the License is distributed on an "AS IS" BASIS,
+rem = WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem = See the License for the specific language governing permissions and
+rem = limitations under the License.
+rem ==========================================================================
+rem
 @REM	Name:   debugconf.bat
 @REM	Author: Shane_Curcuru@us.ibm.com
 @REM Wrapper for running tests without Ant in a debugger
